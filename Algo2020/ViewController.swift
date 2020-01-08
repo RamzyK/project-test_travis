ca02bf8a5ce2b7b89bef452f1e7fd5325e643104
RamzyK <gkermad.ramzy@gmail.com>
2020-01-08 19:36:16 +0100

    initial commit

---
+++ Algo2020/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Algo2020
+//
+//  Created by Ramzy Kermad on 08/01/2020.
+//  Copyright © 2020 Ramzy Kermad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
